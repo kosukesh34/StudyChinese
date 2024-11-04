390ac24edc6716e5ddabf5e867b039685f8b967f
kosukesh34 <kosukesh34@gmail.com>
2024-11-04 12:41:48 +1100

    Initial Commit

---
+++ StudyChineseTests/StudyChineseTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyChineseTests.swift
+//  StudyChineseTests
+//
+//  Created by Kosuke Shigematsu on 11/4/24.
+//
+
+import Testing
+@testable import StudyChinese
+
+struct StudyChineseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
